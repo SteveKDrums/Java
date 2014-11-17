f74c93f8fe2cdf789d26a8dfaf247d26e522ef0f
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-17 19:58:19 +0000

    Prints a simple table

---
+++ Chapter2Ex18.java
@@ -0,0 +1,16 @@
+
+public class Chapter2Ex18 {
+
+	public static void main(String[] args) {
+		// Display table
+		
+		System.out.println("a    b    a + b    a * b  ");
+		System.out.println("1    2    3        2  ");
+		System.out.println("2    3    5        6   ");
+		System.out.println("3    4    7        12   ");
+		System.out.println("4    5    9        20   ");
+		System.out.println("5    6    11       30   ");
+
+	}
+
+}
